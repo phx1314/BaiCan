4cf943a2f9274a5a163891e389311ada20e81928
phx1314 <1163096519@qq.com>
2019-08-14 22:36:09 +0800

    55

--- bc/src/main/java/com/ntdlg/bc/frg/FrgRenzhengxinxi.java
+++ bc/src/main/java/com/ntdlg/bc/frg/FrgRenzhengxinxi.java
@@ -83,11 +83,10 @@ public class FrgRenzhengxinxi extends BaseFrg {
                 loaddata();
                 break;
             case 1:
-//                mBeanSFSM.frontPic = obj.toString();
                 com.ntdlg.bc.F.getUrl(mModelSF.idcard_back_photo, "FrgRenzhengxinxi", 2);
                 break;
             case 2:
-//                mBeanSFSM.backPic = obj.toString();
+                mBeanSFSM.accountId = com.ntdlg.bc.F.UserId;
                 mBeanSFSM.sign = readClassAttr(mBeanSFSM);
                 loadJsonUrl(scanIdentity, new Gson().toJson(mBeanSFSM));
                 break;
@@ -96,12 +95,12 @@ public class FrgRenzhengxinxi extends BaseFrg {
                 break;
             case 120:
                 mModelSF = (ModelSF) obj;
+                mBeanSFSM.accountId = com.ntdlg.bc.F.UserId;
                 mBeanSFSM.idcard_back_photo = mModelSF.idcard_back_photo;
                 mBeanSFSM.idcard_front_photo = mModelSF.idcard_front_photo;
                 mBeanSFSM.pic_photo = mModelSF.pic_photo;
                 mBeanSFSM.sign = readClassAttr(mBeanSFSM);
                 loadJsonUrl(scanIdentity, new Gson().toJson(mBeanSFSM));
-//                com.ntdlg.qd.F.getUrl(mModelSF.idcard_front_photo, "FrgRenzhengxinxi", 1);
                 break;
         }
     }
